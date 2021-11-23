56cc6692d8fed7bffaaa3683e84ea732a0c2291c
nikgitshub <nikhil.phate@ymail.com>
2021-11-23 15:25:54 +0530

    I Have Created The Project Now You Guys Can ADD Files To PROJECT.

---
+++ src/FirstGIT.java
@@ -0,0 +1,14 @@
+
+public class FirstGIT
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(" This Is My First GIT PROJECT ");
+		System.out.println(" This Is Created By LEAD ");
+		System.out.println(" Hello GUYS... ");
+
+		System.out.println(" Tis Is Now In STAGGING Area ");
+	}
+
+}
